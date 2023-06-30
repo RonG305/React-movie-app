5e2aef05ff499a9fc876efaa2ecc99a45d16248b
Ronald <106582548+RonG305@users.noreply.github.com>
2023-06-30 15:44:43 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+
+}
+
